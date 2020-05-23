c0a77d8249b61ba761c3d862e92fea4e624d9cf2
Francisco A. Dominguez <fdominguez@est.frba.utn.edu.ar>
2020-05-23 12:50:15 -0300

    Se consigue un mapa de profundidad, pero malos resultados. Revisar todo

--- ramon_slam3d/src/disparity.cpp
+++ ramon_slam3d/src/disparity.cpp
@@ -3,59 +3,131 @@
 #include <time.h>
 #include <iostream>
 #include <ros/console.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
+#include <stereo_msgs/DisparityImage.h>
+#define MISSING_Z 10000
 
-using namespace cv;
+#include <boost/version.hpp>
+
+inline bool isValidPoint(const cv::Vec3f& pt)
+{
+  // Check both for disparities explicitly marked as invalid (where OpenCV maps pt.z to MISSING_Z)
+  // and zero disparities (point mapped to infinity).
+  return pt[2] != MISSING_Z && !std::isinf(pt[2]);
+}
 
 int main (int argc, char **argv)
 {
     ros::init (argc, argv, "stereo_disparity");
     ros::NodeHandle nh("~");
+    ros::Publisher pub_points2;
+    // In BGR by default
     cv::VideoCapture left("/dev/video4"), right("/dev/video2");
     double width, height;
     double fps;
+    pub_points2 = nh.advertise<sensor_msgs::PointCloud2>("points2",  1);
+    // K: intrinsic camera matrix for the raw (distorted) images
 
+    // D: distortion parameters/coeffs
 
-    // P: Projection matrix
+    // P: Projection/camera matrix
     // XX: Tx for right camera, 0 for left camera
     // [f_rect, alpha, c_x ,      [1, 0, 0, XX ,
     //    0   ,  f_y , c_y ,  *    0, 1, 0,  0 ,
     //    0   ,   0  ,  1 ]        0, 0, 1,  0];
+    // By convention, this matrix specifies the intrinsic (camera) matrix
+    //  of the processed (rectified) image. That is, the left 3x3 portion
+    //  is the normal camera intrinsic matrix for the rectified image.
+    // It projects 3D points in the camera coordinate frame to 2D pixel
+    //  coordinates using the focal lengths (fx', fy') and principal point
+    //  (cx', cy') - these may differ from the values in K.
+    // For monocular cameras, Tx = Ty = 0. Normally, monocular cameras will
+    //  also have R = the identity and P[1:3,1:3] = K.
+    // For a stereo pair, the fourth column [Tx Ty 0]' is related to the
+    //  position of the optical center of the second camera in the first
+    //  camera's frame. We assume Tz = 0 so both cameras are in the same
+    //  stereo image plane. The first camera always has Tx = Ty = 0. For
+    //  the right (second) camera of a horizontal stereo pair, Ty = 0 and
+    //  Tx = -fx' * B, where B is the baseline between the cameras.
+    // Given a 3D point [X Y Z]', the projection (x, y) of the point onto
+    //  the rectified image is given by:
+    //  [u v w]' = P * [X Y Z 1]'
+    //         x = u / w
+    //         y = v / w
+    // This holds for both images of a stereo pair.
+
+
+    // R: Rectification matrix, for stereo cameras only.
+    // A rotation matrix aligning the camera coordinate system to the ideal
+    //  stereo image plane so that epipolar lines in both stereo images are
+    //  parallel.
 
     /* FIXME: Read data from yaml files */
-    // From 20200318_320x240
+    // From 20200327_320x240
     // Right camera
-    cv::Mat K_right = (cv::Mat_<double>(3,3) << 442.95757,    0.     ,  165.6904 ,
-                                                  0.     ,  444.10807,   98.11422,
+    cv::Mat K_right = (cv::Mat_<double>(3,3) << 435.736  ,    0.     ,  163.48678,
+                                                  0.     ,  436.74973,  117.12178,
                                                   0.     ,    0.     ,    1.     );
 
-    cv::Mat P_right = (cv::Mat_<double>(3,4) << 502.87734,    0.     ,  208.39025,  -31.41567,
-                                                  0.     ,  502.87734,   88.63361,    0.     ,
+    cv::Mat D_right = (cv::Mat_<double>(1,5) << -0.139272, 0.228863, 0.000443, 0.002089, 0.000000);
+
+    cv::Mat P_right = (cv::Mat_<double>(3,4) << 453.19158,    0.     ,  159.7411 ,  -27.55073,
+                                                  0.     ,  453.19158,  110.3157 ,    0.     ,
                                                   0.     ,    0.     ,    1.     ,    0.     );
 
-    cv::Mat R_right = (cv::Mat_<double>(3,3) << 0.9964599 , -0.03414671, -0.07682227,
-                                                0.03438375,  0.99940715,  0.00176472,
-                                                0.07671646, -0.00439991,  0.99704324);
-
-    cv::Mat D_right = (cv::Mat_<double>(1,5) << -0.101437, -0.016751, -0.010302, 0.003702, 0.000000);
+    cv::Mat R_right = (cv::Mat_<double>(3,3) << 0.99997044, -0.00746816,  0.00182902,
+                                                0.00746328,  0.99996861,  0.00266238,
+                                               -0.00184884, -0.00264865,  0.99999478);
 
     // Left camera
-    cv::Mat K_left = (cv::Mat_<double>(3,3) << 432.60559,    0.     ,  167.65885,
-                                                 0.     ,  433.84268,   81.72285,
+    cv::Mat K_left = (cv::Mat_<double>(3,3) << 431.44958,    0.     ,  151.48809,
+                                                 0.     ,  432.60045,  103.73964,
                                                  0.     ,    0.     ,    1.     );
 
-    cv::Mat P_left = (cv::Mat_<double>(3,4) << 502.87734,    0.     ,  208.39025,    0.     ,
-                                                 0.     ,  502.87734,   88.63361,    0.     ,
+    cv::Mat D_left = (cv::Mat_<double>(1,5) << -0.088844, -0.120207, 0.001345, -0.002007, 0.000000);
+
+    cv::Mat P_left = (cv::Mat_<double>(3,4) << 453.19158,    0.     ,  159.7411 ,    0.     ,
+                                                 0.     ,  453.19158,  110.3157 ,    0.     ,
                                                  0.     ,    0.     ,    1.     ,    0.     );
 
-    cv::Mat R_left = (cv::Mat_<double>(3,3) <<  0.99559484, -0.03553375, -0.08676556,
-                                                0.0352656 ,  0.99936729, -0.00462175,
-                                                0.0868749 ,  0.00154155,  0.99621804);
+    cv::Mat R_left = (cv::Mat_<double>(3,3) <<   0.99989833, -0.0080206 , -0.01178974,
+                                                 0.00798926,  0.99996443, -0.0027027 ,
+                                                 0.011811  ,  0.00260823,  0.99992685);
 
-    cv::Mat D_left = (cv::Mat_<double>(1,5) << -0.093483, -0.009592, -0.009334, 0.000425, 0.000000);
+    double c_x = P_left.at<double>(0,2);
+    double c_y = P_left.at<double>(1,2);
+    double f = P_left.at<double>(1,1);
+    double cr_x = P_right.at<double>(0,2);
+
+    // Q: reprojection matrix, defined as
+    // [1, 0,   0  ,     -c_x       ,
+    //  0, 1,   0  ,     -c_y       ,
+    //  0, 0,   0  ,       f        ,
+    //  0, 0, -1/Tx, (c_x-c'_x)/T_x];
+    // Here the parameters are from the left image except for c'_x
+
+    float T_x = P_right.at<double>(0,3) / P_right.at<double>(0,0);
+
+    double Q03, Q13, Q23, Q32, Q33;
+    Q03 = -c_x;
+    Q13 = -c_y;
+    Q23 = f;
+    Q32 = -1/T_x;
+    Q33 = (c_x - cr_x)/T_x;
+    double data[16] = {1, 0,  0 , Q03,
+                       0, 1,  0 , Q13,
+                       0, 0,  0 , Q23,
+                       0, 0, Q32, Q33}; 
+    cv::Mat Q(4, 4, CV_64F, data);
 
     width = 320;
     height = 240;
     fps = 30.0;
+
+    cv::Size image_size;
+    image_size.height = height;
+    image_size.width = width;
 
     // Resize images
     left.set(cv::CAP_PROP_FRAME_WIDTH, width);
@@ -68,14 +140,34 @@ int main (int argc, char **argv)
     right.set(cv::CAP_PROP_FPS, fps);
 
     // Use MJPEG to avoid overloading the USB 2.0 bus
-    // left.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
-    // right.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+    left.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+    right.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
 
-    // Learning OpenCV 3 p. 710 aprox
-    cv::Size image_size;
-    image_size.height = height;
-    image_size.width = width;
+    // Grab one to get frame info
+    cv::Mat imgL, imgR;
+    if(!(left.read(imgL) && right.read(imgR)))
+    {
+        ROS_ERROR("Unable to grab some CAM");
+        left.release();
+        right.release();
+        return -1;
+    }
 
+    if(imgL.size() != imgR.size() || imgL.type() != imgR.type())
+    {
+        ROS_ERROR("The cameras use different framesize or type");
+        left.release();
+        right.release();
+        return -1;
+    }
+    
+    // ENSURE RIGHT TYPE AND SIZE
+    image_size = imgL.size();
+    // this will hold images from both cams 
+    cv::Mat frame(cv::Size(2 * image_size.width, image_size.height), imgL.type());
+    // define ROI to compose mosaic
+    cv::Mat frameL(frame(cv::Rect(0, 0, image_size.width, image_size.height)));
+    cv::Mat frameR(frame(cv::Rect(image_size.width, 0, image_size.width, image_size.height)));
 
     // Steps:
     // 1) Image raw
@@ -83,31 +175,69 @@ int main (int argc, char **argv)
     // 3) Rectify
     // 4) Crop
 
-    // Build the undistort map which we will use for all subsequent frames. //
+    // Build the undistort map which we will use for all subsequent frames.
+    // p. 762 OpenCV 3 book
+    // For R, use Rl and Rr from cv::stereoRectify(); for cameraMatrix, 
+    // use cameraMatrix1 or cameraMatrix2. For newCameraMatrix we could use 
+    // the first three columns of the 3 × 4 Pl or Rr from cv::stereoRectify(), but as a
+    // convenience the function allows us to pass Pl or Pr directly and it will read 
+    // newCamera Matrix from them
+    /* FIXME: CV_16SC2 or CV_32C1? */
     cv::Mat map1_right, map2_right;
-    cv::initUndistortRectifyMap( K_right, D_right, cv::Mat(),
-    K_right, image_size, CV_16SC2, map1_right, map2_right
+    cv::initUndistortRectifyMap(K_right, D_right, R_right,
+    P_right, image_size, CV_16SC2, map1_right, map2_right
     );
 
     cv::Mat map1_left, map2_left;
-    cv::initUndistortRectifyMap( K_left, D_left, cv::Mat(),
-    K_left, image_size, CV_16SC2, map1_left, map2_left
+    cv::initUndistortRectifyMap(K_left, D_left, R_left,
+    P_left, image_size, CV_16SC2, map1_left, map2_left
     );
 
+    // Disparity image algorithm initialization
+    // Elegir buenos parametros!!!!
+    // int minDisparity = -64;
+    // int numDisparities = 128;
+    // int blockSize = 11;
+    // int P1 = 100;
+    // int P2 = 1000;
+    // int disp12MaxDiff = 32;
+    // int preFilterCap = 0;
+    // int uniquenessRatio = 15;
+    // int speckleWindowSize = 1000;
+    // int speckleRange = 16;
+    // int mode = cv::StereoSGBM::MODE_HH;
+    int minDisparity = 0;
+    int numDisparities = 128;
+    int blockSize = 5;
+    int P1 = 0;
+    int P2 = 0;
+    int disp12MaxDiff = 0;
+    int preFilterCap = 31;
+    int uniquenessRatio = 15;
+    int speckleWindowSize = 500;
+    int speckleRange = 4;
+    int mode = cv::StereoSGBM::MODE_HH;
+    cv::Ptr<cv::StereoSGBM> stereo = cv::StereoSGBM::create(minDisparity, numDisparities, blockSize, 
+                                                            P1, P2, 
+                                                            disp12MaxDiff, preFilterCap, uniquenessRatio, 
+                                                            speckleWindowSize, speckleRange, 
+                                                            mode);
+    
+
     ros::Rate r(1000.0);
-    // Grab both frames first, then retrieve to minimize latency between cameras
     while(nh.ok())
     {
-        ros::spinOnce();
+        // Grab both frames first, then retrieve to minimize latency between cameras
         if(!(left.grab() && right.grab()))
         {
             ROS_ERROR("Unable to grab from one or both cameras");
             break;
         }
 
-        cv::Mat right_buff, left_buff, right_buff_c1, left_buff_c1;
-        right.retrieve(right_buff);
+        cv::Mat right_buff, left_buff;
+        cv::Mat right_buff_rect, left_buff_rect;
         left.retrieve(left_buff);
+        right.retrieve(right_buff);
 
         if(right_buff.empty() || left_buff.empty())
         {
@@ -115,76 +245,93 @@ int main (int argc, char **argv)
             break;
         }
 
+        // 2) 3) Undistort and Rectify images
+        cv::remap(left_buff, left_buff_rect, map1_left, map2_left, cv::INTER_LINEAR);
+        cv::remap(right_buff, right_buff_rect, map1_right, map2_right, cv::INTER_LINEAR);
 
-        // cv::remap(right_buff, right_buff, map1_right, map2_right, cv::INTER_LINEAR, cv::BORDER_CONSTANT, cv::Scalar());
-        // cv::remap(left_buff, left_buff, map1_left, map2_left, cv::INTER_LINEAR, cv::BORDER_CONSTANT, cv::Scalar());
+        cv::Mat disparity, vdisparity;
 
-        // 2) Undistort images
-        cv::undistort(left_buff, left_buff_c1, K_left, D_left, K_left);
-        cv::cvtColor(left_buff_c1, left_buff_c1, CV_BGR2GRAY);
-        cv::undistort(right_buff, right_buff_c1, K_right, D_right, K_right);
-        cv::cvtColor(right_buff_c1, right_buff_c1, CV_BGR2GRAY);
+        stereo->compute(left_buff_rect, right_buff_rect, disparity);
+        cv::normalize(disparity, vdisparity, 0, 256, cv::NORM_MINMAX, CV_8U);
 
-        // 3) Rectify
-        // The rotation R is the identity because we don't want to rotate the image
-        cv::Mat3f R;
-        R << 1, 0, 0,
-             0, 1, 0,
-             0, 0, 1;
+        if(left_buff_rect.size() != vdisparity.size() || right_buff_rect.size() != vdisparity.size())
+        {
+            ROS_ERROR("Rectified images and disparity image have different sizes");
+            return -1;
+        }
+        // Get depth map from disparity
+        cv::Mat_<cv::Vec3f> depth(vdisparity.size(), CV_32FC3);
+
+        /* FIXME: Check if handleMissingValues is required */
+        cv::reprojectImageTo3D(vdisparity, depth, Q);
+
+        sensor_msgs::PointCloud2Ptr points_msg = boost::make_shared<sensor_msgs::PointCloud2>();
+        points_msg->header.stamp = ros::Time::now();
+        points_msg->header.frame_id = "map";
+        points_msg->height = depth.rows;
+        points_msg->width = depth.cols;
+        points_msg->is_bigendian = false;
+        points_msg->is_dense = false;
+
+        // Used to conveniently set the fields structure, resize or clear a point cloud.
+        sensor_msgs::PointCloud2Modifier pcd_modifier(*points_msg);
+        // This call also resizes the data structure according to the given width, height and fields
+        pcd_modifier.setPointCloud2FieldsByString(2, "xyz", "rgb");
+
+        // Used to iterate the fields (like "x" for the x-coordinate, or "r" for the red color)
+        sensor_msgs::PointCloud2Iterator<float> iter_x(*points_msg, "x");
+        sensor_msgs::PointCloud2Iterator<float> iter_y(*points_msg, "y");
+        sensor_msgs::PointCloud2Iterator<float> iter_z(*points_msg, "z");
+        sensor_msgs::PointCloud2Iterator<uint8_t> iter_r(*points_msg, "r");
+        sensor_msgs::PointCloud2Iterator<uint8_t> iter_g(*points_msg, "g");
+        sensor_msgs::PointCloud2Iterator<uint8_t> iter_b(*points_msg, "b");
+
+        float bad_point = std::numeric_limits<float>::quiet_NaN ();
+        for (int v = 0; v < depth.rows; ++v)
+        {
+
+            for (int u = 0; u < depth.cols; ++u, ++iter_x, ++iter_y, ++iter_z)
+            {
+                if (isValidPoint(depth(v,u)))
+                {
+                    // x,y,z
+                    *iter_x = depth(v, u)[0];
+                    *iter_y = depth(v, u)[1];
+                    *iter_z = depth(v, u)[2];
+                }
+                else
+                {
+                    *iter_x = *iter_y = *iter_z = bad_point;
+                }
+            }
+        }
+
+        // Fill in color (BGR8)
+        const cv::Mat_<cv::Vec3b> color(left_buff_rect.rows, left_buff_rect.cols,
+                                        (cv::Vec3b*) &left_buff.data[0],
+                                        left_buff_rect.step);
+
+        for (int v = 0; v < color.rows; ++v)
+        {
+            for (int u = 0; u < color.cols; ++u, ++iter_r, ++iter_g, ++iter_b)
+            {
+                const cv::Vec3b &bgr = color(v,u);
+                *iter_r = bgr[2];
+                *iter_g = bgr[1];
+                *iter_b = bgr[0];
+            }
+        }
         
-        cv::Vec3f T;
-        T << 0,
-             0,
-             0;
+        left_buff_rect.copyTo(frameL);
+        right_buff_rect.copyTo(frameR);
+        cv::imshow("camera_frame", frame);
+        cv::imshow("disparity", vdisparity);
+        cv::imshow("depth", depth);
 
-        float T_x = P_right.at<float>(0,3) / P_right.at<float>(0,0);
-        // Q: reprojection matrix, defined as
-        // [1, 0,   0  ,     -c_x       ,
-        //  0, 1,   0  ,     -c_y       ,
-        //  0, 0,   0  ,       f        ,
-        //  0, 0, -1/Tx, (c_x-c'_x)/T_x];
-        // Here the parameters are from the left image except for c'_x
-        cv::Mat4f Q;
-        Q << 1, 0,   0   ,              -P_left.at<float>(0,2)                 ,
-             0, 1,   0   ,              -P_left.at<float>(0,5)                 ,
-             0, 0,   0   ,              -P_left.at<float>(0,0)                 ,
-             0, 0, -1/T_x, (P_left.at<float>(0,2) - P_right.at<float>(0,2))/T_x;
-
-        cv::stereoRectify(K_left, D_left, K_right, D_right, image_size, 
-                          R, T, R_left, R_right, P_left, P_right, Q);
-
-
-
-
-        cv::Mat disparity;
-        cv::Ptr<StereoBM> sbm = cv::StereoBM::create();
-        // sbm->setPreFilterSize(9);
-        // sbm->setPreFilterCap(31);
-        // sbm->setMinDisparity(0);
-        // sbm->setNumDisparities(64);
-        // sbm->setTextureThreshold(10);
-        // sbm->setUniquenessRatio(0);
-        // sbm->setSpeckleWindowSize(100);
-        // sbm->setSpeckleRange(4);
-        // sbm->setDisp12MaxDiff(1);
-        sbm->setPreFilterSize(9);
-        sbm->setPreFilterCap(61);
-        sbm->setMinDisparity(-39);
-        sbm->setNumDisparities(64);
-        sbm->setTextureThreshold(500);
-        sbm->setUniquenessRatio(0);
-        sbm->setSpeckleWindowSize(100);
-        sbm->setSpeckleRange(4);
-        sbm->setDisp12MaxDiff(1);
-        sbm->compute(left_buff_c1,right_buff_c1,disparity);
-        cv::normalize(disparity, disparity, 0, 255, CV_MINMAX, CV_8U);
-
-        cv::imshow("left", left_buff_c1);
-        cv::imshow("right", right_buff_c1);
-        cv::imshow("disparity",disparity);
-        waitKey(1);
-        
-        r.sleep();
+        pub_points2.publish(points_msg);
+        cv::waitKey(1);
+        ros::spinOnce();
+        // r.sleep();
     }
 
     left.release();
